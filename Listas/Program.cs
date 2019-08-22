f55a1952ab626cd77ca4dd5a741e7b039f04ed8e
alumno <alumno@E328>
2019-08-22 08:40:51 -0700

    Agregar archivos de proyecto.

---
+++ Listas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colores =
+                new List<string>();
+
+            colores.Add("azul");
+            colores.Add("verde");
+            colores.Add("violeta");
+            colores.Add("amarillo");
+
+            colores.RemoveAt(1);
+            colores.Remove("amarillo");
+
+            Console.WriteLine("numero de elementos:" + colores.Count);
+
+            int indiceColor = 1;
+
+            foreach (string color in colores)
+            {
+                Console.WriteLine("Elemento:"+indiceColor.ToString () + ": " + color);
+            }
+
+            Console.Read();
+        }
+    }
+}
